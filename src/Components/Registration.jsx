3be08053a93ff875f073460671d06fafb4544bd9
Nazmul Hoque <nazmul.info.duet@gmail.com>
2025-11-12 19:07:57 +0600

    Design Error page, Registration page, login page

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -1,9 +1,88 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router";
 const Registration = () => {
+  const [showEye, setShowEye] = useState(false);
+  const handleEyeBtn = () => {
+    setShowEye(!showEye);
+  };
   return (
-    <div>
-      <h1>registration</h1>
+    <div className="card bg-base-100 mx-auto my-8 w-full max-w-md shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-center lg:text-left text-5xl font-semibold py-4">
+          Registration now!
+        </h1>
+        <form>
+          <fieldset className="fieldset">
+            <label className="label">Name</label>
+            <input type="text" className="input w-full" placeholder="Name" />
+            <label className="label">Photo URL</label>
+            <input
+              type="url"
+              className="input w-full"
+              placeholder="Photo URL"
+            />
+            <label className="label">Email</label>
+            <input type="email" className="input w-full" placeholder="Email" />
+            <label className="label">Password</label>
+            <div className="relative">
+              <input
+                type={showEye ? "text" : "password"}
+                className="input w-full"
+                placeholder="Password"
+              />
+              <button
+                type="button"
+                onClick={handleEyeBtn}
+                className=" absolute top-[15px] right-[25px] z-40"
+              >
+                {showEye ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+            <button className="btn btn-neutral mt-4">Registration</button>
+          </fieldset>
+        </form>
+        {/* Google */}
+        <button className="btn bg-white text-black border-[#e5e5e5]">
+          <svg
+            aria-label="Google logo"
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <g>
+              <path d="m0 0H512V512H0" fill="#fff"></path>
+              <path
+                fill="#34a853"
+                d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+              ></path>
+              <path
+                fill="#4285f4"
+                d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+              ></path>
+              <path
+                fill="#fbbc02"
+                d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+              ></path>
+              <path
+                fill="#ea4335"
+                d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+              ></path>
+            </g>
+          </svg>
+          SignUp with Google
+        </button>
+        <p className="text-xl">
+          Don't have any account?
+          <Link
+            className="text-[#472dd4] font-semibold hover:underline"
+            to={"/login"}
+          >
+            Login Now!
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router";
+import error from "../assets/error.png";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>error page</h1>
+    <div className="flex flex-col justify-center items-center w-full h-[90vh]">
+      {/* {useTitle("Error Page")} */}
+      <figure>
+        <img src={error} alt="" />
+      </figure>
+      <div className="w-[60%] text-center py-6">
+        <h2 className="text-3xl font-bold uppercase ">404 - page not found</h2>
+        <p className="py-2">
+          The page you are looking for might have been remove had its name
+          changed or is temporarily unavailable.{" "}
+        </p>
+        <button className="mt-3 px-8 py-4 bg-[#0000c5] uppercase rounded-full text-2xl text-white font-semibold">
+          <Link to={"/"}>go to homepage</Link>
+        </button>
+      </div>
     </div>
   );
 };
